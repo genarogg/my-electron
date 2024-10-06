e8b8e3e44ccfe95f61a5bfb40b5bdb66e9eddee5
Genaro Gonzalez <genarrogg@gmail.com>
2024-10-06 19:28:24 -0400

    tratando de instalar devtoll

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,26 +1,31 @@
 import { app, shell, BrowserWindow } from "electron";
 import { join } from "path";
 import { electronApp, optimizer, is } from "@electron-toolkit/utils";
-import icon from "../../resources/icon.png?asset";
+import installExtension, {
+  REACT_DEVELOPER_TOOLS,
+} from "electron-devtools-installer";
 
+import icon from "../../resources/icon.png?asset";
+import path from "path";
 import router from "./routers/index";
 
-function createWindow(): void {
+async function createWindow(): Promise<void> {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
-    show: false,
+    height: 720,
+    width: 1280,
+    show: true,
+    // frame: false,
+    // transparent: true,
     autoHideMenuBar: true,
+    title: "e.t. ademar vasquez chavez",
+    icon: path.join(__dirname, "../../resources/logo.ico"),
     ...(process.platform === "linux" ? { icon } : {}),
     webPreferences: {
       preload: join(__dirname, "../preload/index.js"),
+      // nodeIntegration: true,
       sandbox: false,
     },
-  });
-
-  mainWindow.on("ready-to-show", () => {
-    mainWindow.show();
   });
 
   mainWindow.webContents.setWindowOpenHandler((details) => {
@@ -28,8 +33,6 @@ function createWindow(): void {
     return { action: "deny" };
   });
 
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env["ELECTRON_RENDERER_URL"]) {
     mainWindow.loadURL(process.env["ELECTRON_RENDERER_URL"]);
   } else {
@@ -37,40 +40,35 @@ function createWindow(): void {
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  // Set app user model id for windows
+app.whenReady().then(async () => {
   electronApp.setAppUserModelId("com.electron");
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
   app.on("browser-window-created", (_, window) => {
     optimizer.watchWindowShortcuts(window);
   });
 
-  // IPC test
+  // cargando router
   router;
 
   createWindow();
 
   app.on("activate", function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
+
+  if (is.dev) {
+    try {
+      const name = await installExtension(REACT_DEVELOPER_TOOLS);
+      console.log(`Added Extension:  ${name}`);
+    } catch (err) {
+      console.log("An error occurred: ", err);
+    }
+  }
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// cierra la ventana en todas las plataformas
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,35 +1,31 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+interface AppProps {}
 
-function App(): JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+// const App: React.FC<AppProps> = () => {
+//   return (
+//     <>
+//       <Provider>
+//         <Router>
+//           <Routes>
+//             <Route path="/" element={<Login />} />
+//             <Route path="/dashboard" element={<Dashboard />} />
+//           </Routes>
+//         </Router>
+//       </Provider>
+//     </>
+//   );
+// };
 
+const App: React.FC<AppProps> = () => {
   return (
     <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
+      <Router>
+        <Routes>
+          <Route path="/" element={<p>hola</p>} />
+        </Routes>
+      </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
