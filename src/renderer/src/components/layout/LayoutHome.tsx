9937505a2f437d26032792a25d76cce6433f918b
Genaro Gonzalez <genarrogg@gmail.com>
2024-10-06 23:43:16 -0400

    eliminado codigo no utilizado

--- src/renderer/src/components/layout/LayoutHome.tsx
+++
@@ -1,9 +0,0 @@
-interface LayoutHomeProps {
-  children: React.ReactNode;
-}
-
-const LayoutHome: React.FC<LayoutHomeProps> = ({ children }) => {
-  return <>{children}</>;
-};
-
-export default LayoutHome;
